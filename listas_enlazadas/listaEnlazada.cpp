0fc3a128dbe4650ab61c017e300f4b29451ccb5d
juandgg <juanguiza65@gmail.com>
2025-04-08 12:42:30 +0000

    algunos cambios

---
+++ listas_enlazadas/listaEnlazada.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+namespace variablesXIgualA2 {
+    int x =2 ;
+}
+
+
+
+int main (){
+    int x  = 2;
+    int y = 3 ;
+
+    //para salto de linea
+    std::cout<<"HOLA"<<std ::endl;
+    std::cout<<"HOLA OTRA VEZ"<<'\n';
+    
+
+    //suma variables
+
+    std::cout<<x+y << '\n';
+
+    const int noCambia = 2;
+
+    std::cout<<noCambia ;
+
+    return 0 ;
+}
